46d35d391d0b611868b1b4851dd8d225030e5500
DB <barkoczi>
2018-01-15 16:01:56 +0100

    added code to obtain pubmed id

--- WoS.R
+++ WoS.R
@@ -23,44 +23,50 @@ pubs2 <- pubs[-c(select[2,]),]
   print("More than 2 entries found, check code")
 }
 
-#trying to find link to "cited references page" based on the WOS ID of each paper
-sessionID <- "7C205FA0D1F0CF17AFE9D924B20D9BD7"
-WOS_ID <- "WOS:A1968A428000023"
+save(pubs2,file="science.Rdata")
 
 
-WOS_ID <- pubs2$uid[2]
-link <- paste0("http://apps.webofknowledge.com/InterService.do;jsessionid=",sessionID,
-        "?product=WOS&toPID=WOS&action=AllCitationService&isLinks=yes&highlighted_tab=WOS&last_prod=WOS&fromPID=WOS&returnLink=http%3a%2f%2fapps.webofknowledge.com%2ffull_record.do%3fhighlighted_tab%3dWOS%26last_prod%3dWOS%26search_mode%3dAdvancedSearch%26qid%",
-        "3d2%27log_event%3dyes%26product%3dWOS%26SID%3dD62RGf3DTL1AwixErdZ%26viewType%3dfullRecord%26doc%3d1%26page%3d1&srcDesc=RET2WOS&srcAlt=Back+to+Web+of+Science&UT=",
-        WOS_ID,
-        "&search_mode=CitedRefList&SID=D62RGf3DTL1AwixErdZ&parentProduct=WOS&parentQid=2&parentDoc=1&recid=",
-        WOS_ID,
-        "&PREC_REFCOUNT=6&fromRightPanel=true")
-
-
-
-a <-"http://apps.webofknowledge.com/InterService.do?product=WOS&toPID=WOS&action=AllCitationService&isLinks=yes&highlighted_tab=WOS&last_prod=WOS&fromPID=WOS&returnLink=http%3a%2f%2fapps.webofknowledge.com%2ffull_record.do%3fhighlighted_tab%3dWOS%26last_prod%3dWOS%26search_mode%3dAdvancedSearch%26qid%3d60%26log_event%3dyes%26product%3dWOS%26SID%3dD62RGf3DTL1AwixErdZ%26viewType%3dfullRecord%26doc%3d1%26page%3d1&srcDesc=RET2WOS&srcAlt=Back+to+Web+of+Science&UT=WOS:A1970G983200030&search_mode=CitedRefList&SID=D62RGf3DTL1AwixErdZ&parentProduct=WOS&parentQid=60&parentDoc=1&recid=WOS:A1970G983200030&PREC_REFCOUNT=19&fromRightPanel=true"
-b<- "http://apps.webofknowledge.com/InterService.do?product=WOS&toPID=WOS&action=AllCitationService&isLinks=yes&highlighted_tab=WOS&last_prod=WOS&fromPID=WOS&returnLink=http%3a%2f%2fapps.webofknowledge.com%2ffull_record.do%3fhighlighted_tab%3dWOS%26last_prod%3dWOS%26search_mode%3dAdvancedSearch%26qid%3d51%26log_event%3dyes%26product%3dWOS%26SID%3dD62RGf3DTL1AwixErdZ%26viewType%3dfullRecord%26doc%3d1%26page%3d1&srcDesc=RET2WOS&srcAlt=Back+to+Web+of+Science&UT=WOS:A1968A428000023&search_mode=CitedRefList&SID=D62RGf3DTL1AwixErdZ&parentProduct=WOS&parentQid=51&parentDoc=1&recid=WOS:A1968A428000023&PREC_REFCOUNT=6&fromRightPanel=true"
+# 
+# #trying to find link to "cited references page" based on the WOS ID of each paper
+# sessionID <- "7C205FA0D1F0CF17AFE9D924B20D9BD7"
+# WOS_ID <- "WOS:A1968A428000023"
+# 
+# 
+# WOS_ID <- pubs2$uid[2]
+# link <- paste0("http://apps.webofknowledge.com/InterService.do;jsessionid=",sessionID,
+#         "?product=WOS&toPID=WOS&action=AllCitationService&isLinks=yes&highlighted_tab=WOS&last_prod=WOS&fromPID=WOS&returnLink=http%3a%2f%2fapps.webofknowledge.com%2ffull_record.do%3fhighlighted_tab%3dWOS%26last_prod%3dWOS%26search_mode%3dAdvancedSearch%26qid%",
+#         "3d2%27log_event%3dyes%26product%3dWOS%26SID%3dD62RGf3DTL1AwixErdZ%26viewType%3dfullRecord%26doc%3d1%26page%3d1&srcDesc=RET2WOS&srcAlt=Back+to+Web+of+Science&UT=",
+#         WOS_ID,
+#         "&search_mode=CitedRefList&SID=D62RGf3DTL1AwixErdZ&parentProduct=WOS&parentQid=2&parentDoc=1&recid=",
+#         WOS_ID,
+#         "&PREC_REFCOUNT=6&fromRightPanel=true")
+# 
+# 
+# 
+# a <-"http://apps.webofknowledge.com/InterService.do?product=WOS&toPID=WOS&action=AllCitationService&isLinks=yes&highlighted_tab=WOS&last_prod=WOS&fromPID=WOS&returnLink=http%3a%2f%2fapps.webofknowledge.com%2ffull_record.do%3fhighlighted_tab%3dWOS%26last_prod%3dWOS%26search_mode%3dAdvancedSearch%26qid%3d60%26log_event%3dyes%26product%3dWOS%26SID%3dD62RGf3DTL1AwixErdZ%26viewType%3dfullRecord%26doc%3d1%26page%3d1&srcDesc=RET2WOS&srcAlt=Back+to+Web+of+Science&UT=WOS:A1970G983200030&search_mode=CitedRefList&SID=D62RGf3DTL1AwixErdZ&parentProduct=WOS&parentQid=60&parentDoc=1&recid=WOS:A1970G983200030&PREC_REFCOUNT=19&fromRightPanel=true"
+# b<- "http://apps.webofknowledge.com/InterService.do?product=WOS&toPID=WOS&action=AllCitationService&isLinks=yes&highlighted_tab=WOS&last_prod=WOS&fromPID=WOS&returnLink=http%3a%2f%2fapps.webofknowledge.com%2ffull_record.do%3fhighlighted_tab%3dWOS%26last_prod%3dWOS%26search_mode%3dAdvancedSearch%26qid%3d51%26log_event%3dyes%26product%3dWOS%26SID%3dD62RGf3DTL1AwixErdZ%26viewType%3dfullRecord%26doc%3d1%26page%3d1&srcDesc=RET2WOS&srcAlt=Back+to+Web+of+Science&UT=WOS:A1968A428000023&search_mode=CitedRefList&SID=D62RGf3DTL1AwixErdZ&parentProduct=WOS&parentQid=51&parentDoc=1&recid=WOS:A1968A428000023&PREC_REFCOUNT=6&fromRightPanel=true"
 
 
 #Get missing DOI information by matching web of science titles with CrossRef 
 library(rcrossref)
-retreive_doi=cr_works(filter = c(issn=c('0036-8075')),flq = c(query.title=pubs$title[16]))
+retreive_doi=cr_works(filter = c(issn=c('0036-8075')),flq = c(query.title=pubs$title[1]))
 
-retreive_doi[[2]]$DOI[1]
-
-
+doi <- retreive_doi[[2]]$DOI[1]
 
 
+
+#########################################
 #Get abstract for a paper using SCOPUS
+#########################################
 
 library(rscopus)
-set_api_key("c2c55699f1c9d5642e2e0c744291fdc5") #you can get your own API key by registering on the SCOPUS website
+set_api_key("fe18beed32b3e133b6b157ec0e455266") #you can get your own API key by registering on the SCOPUS website
 
-example_doi <- "10.1007/978-3-319-64310-6_13"
+example_doi <- '4321261'
+
 
 paper <- abstract_retrieval(example_doi,
-                            identifier = c("doi"), 
+                            identifier = c("pubmed_id"), 
                             http_end = NULL)
 
 #get abstract
@@ -73,5 +79,31 @@ paper$content$`abstracts-retrieval-response`$authkeywords
 #get references
 paper$content$`abstracts-retrieval-response`$item
 
+########################################################################################
+#since the DOI information is not enough for scopus, we need a way to obtain pubmed IDs.
+########################################################################################
+
+library(RISmed)
+
+#search journal and publication type on Pubmed
+search_topic <- '("Science"[Journal]) AND "journal article"[Publication Type]'
+
+#not sure yet how to get all results
+search_query <- EUtilsSummary(search_topic, 
+                              retstart=0,
+                              retmax=10000, 
+                              mindate=1900,
+                              maxdate=2018)
+#get PUBMED ID for each entry
+QueryId(search_query)
+
+
+
+#alternatively search for specific title
+search_topic <- 'REPRESSION OF INTERFERON ACTION - INDUCED DEDIFFERENTIATION OF EMBRYONIC CELLS[Title]'
+search_query <- EUtilsSummary(search_topic, retstart=0,retmax=1000, mindate=1900,maxdate=2018)
+
+#get pubmed ID for that entry
+QueryId(search_query)
 
 
