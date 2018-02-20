d5b445fd0985c616f98eac2f14dd02401cb4515c
Martin Arvidsson <marar08@liu.se>
2018-02-20 14:52:03 +0100

    .

--- WoS_main.R
+++ WoS_main.R
@@ -46,87 +46,19 @@ issns <- unlist(list(S_issn,SS_issn,H_issn,ES_issn))
 
 
 
-# Get doi's via cross ref
+# (2) Get doi's via cross ref
 # ------------------------------------------
 
 # Example (Science dois)
 system.time(science_dois <- get_dois(issn = '0036-8075',
-                         from_year = 2000, 
-                         to_year = 2017))
+                                     from_year = 2000, 
+                                     to_year = 2017))
 science_dois
 # ------------------------------------------
 
 
 
-# (3) Translate between DOI and Pubmed ID
-# ------------------------------------------
-
-# APPROACH 1
-# ===========
-out <- alm_ids(doi='10.1371/journal.pone.0029797', info='detail')
-out <- alm_ids(doi=dois_dt[1:1000]$doi)
-dois_dt$doi[2]
-# ===========
-
-
-# APPROACH 2
-# ===========
-# Standard
-pubmed_dt <- lapply(dois_dt[1:10]$doi, function(d) get_pubmedid(doi = d))
-pubmed_dt <- rbindlist(pubmed_dt)
-# Parallel (faster?)
-dois <- dois_dt[1:1000]$doi
-cl <- parallel::makeCluster(7)
-varlist <- c("data.table","get_pubmedid","entrez_search","dois")
-parallel::clusterExport(cl = cl,varlist = varlist,envir = environment())
-system.time(pubmed_dt <- parLapply(cl = cl,
-                                   X = dois,
-                                   fun = function(x) try(get_pubmedid(doi = x))))
-parallel::stopCluster(cl)
-pubmed_dt <- rbindlist(pubmed_dt)
-pubmed_dt[!is.na(pubmedid)]
-# ===========
-# ------------------------------------------
-
-
-
-# (4) Get article, references, etc. via scopus
-# ------------------------------------------
-# Set api key
-set_api_key("c2c55699f1c9d5642e2e0c744291fdc5")
-
-# Examle (DOI)
-article <- rscopus::article_retrieval(id = science_dois$doi[5],
-                                      identifier = c("doi"),
-                                      http_end = NULL)
-abstract <- rscopus::abstract_retrieval(id = science_dois$doi[5],
-                                        identifier = c("doi"),
-                                        http_end = NULL)
-
-# Example (PUBMEDID)
-article <- rscopus::article_retrieval(id = pubmed_dt[!is.na(pubmedid),]$pubmedid[5],
-                                      identifier = c("pubmed_id"),
-                                      http_end = NULL)
-abstract <- rscopus::abstract_retrieval(id = pubmed_dt[!is.na(pubmedid),]$pubmedid[5],
-                                        identifier = c("pubmed_id"),
-                                        http_end = NULL)
-
-# Using function (outdated)
-#abstracts <- lapply(science_dois$doi,function(d) get_abstract(doi = d))
-# ------------------------------------------
-
-
-
-# (5) Get abstracts via entrez
-# ------------------------------------------
-#https://cran.r-project.org/web/packages/rentrez/vignettes/rentrez_tutorial.html
-raw_abs <- entrez_fetch(db="pubmed", id= pubmed_dt[!is.na(pubmedid),]$pubmedid[5], rettype="abstract")
-cat(raw_abs)
-# ------------------------------------------
-
-
-
-# (6) Get article information
+# (3) Get article information
 # -----------------------------------
 
 # Set api key
@@ -134,20 +66,24 @@ api_key <- "c2c55699f1c9d5642e2e0c744291fdc5"
 set_api_key(api_key)
 
 # For one
-article_info <- get_article_info(doi = science_dois$doi[5])
+# =============
+#article_info <- get_article_info(doi = science_dois$doi[5])
 
 # For multiple
-article_info <- lapply(science_dois$doi[1:2],function(d) try(get_article_info(doi = d)))
+# =============
+#article_info <- lapply(science_dois$doi[1:2],function(d) try(get_article_info(doi = d)))
 
 # For multiple (parallel)
-cl <- parallel::makeCluster(7)
+# =============
+ncores <- detectCores() - 1
+cl <- parallel::makeCluster(ncores)
 varlist <- c("data.table","get_article_info","get_basics","get_authors_dt",
              "get_reference_dt","get_abstract","abstract_retrieval","set_api_key",
              "api_key","rbindlist","setcolorder")
 parallel::clusterExport(cl = cl,varlist = varlist,envir = environment())
 system.time(ai_dt <- parLapply(cl = cl,
-                                   X = science_dois$doi,
-                                   fun = function(d) try(get_article_info(doi = d,api_key = api_key))))
+                               X = science_dois$doi,
+                               fun = function(d) try(get_article_info(doi = d,api_key = api_key))))
 parallel::stopCluster(cl)
 a <- sapply(ai_dt,is.list)
 a <- which(a==FALSE)
