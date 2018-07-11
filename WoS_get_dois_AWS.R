7b7f2753462bf3280e4485bb986cdf2185a84ce5
Daniel B <dnlbrkc@gmail.com>
2018-07-11 13:44:02 +0200

    modified code for running on AWS

---
+++ WoS_get_dois_AWS.R
@@ -0,0 +1,38 @@
+library(data.table)
+library(rcrossref)
+library(dplyr)
+
+
+source("WoS_functions.R")
+load("store.Rdata")
+
+# Split into batches
+issn_vector <- store$issn
+n_cores <- 6
+chunk_size <- length(issn_vector)/n_cores
+store_batches <- split(issn_vector, ceiling(seq_along(issn_vector)/chunk_size))
+
+#change this for each machine
+machine_id <- 1
+
+# Collect
+store_dois <- list()
+for(i in machine_id){
+  #i = 1
+  temp_dois <- list()
+  for(j in 1:length(store_batches[[i]])){
+    #j=1  
+    # Process
+    temp_dois[[j]] <- get_dois(issn = store_batches[[i]][j],from_year = 1900,to_year = 2017)
+    print(j)
+  }
+  # Add to big list
+  temp_dois <- temp_dois[sapply(temp_dois,is.data.table)]
+  temp_dois <- rbindlist(temp_dois)
+  store_dois <- rbindlist(list(store_dois,temp_dois))
+  # Print status
+  print(i)
+  # Export
+  saveRDS(object = temp_dois,file = paste0('dois_',i,'.rds'))
+  rm(temp_dois)
+}
