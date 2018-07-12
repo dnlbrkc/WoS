aa27bbd37b64506a3d10ccbcc1c9e48022c74df3
Daniel Barkoczi <dnlbrkc@gmail.com>
2018-07-12 10:53:27 +0200

    Update WoS_get_dois_AWS.R

--- WoS_get_dois_AWS.R
+++ WoS_get_dois_AWS.R
@@ -17,22 +17,23 @@ machine_id <- 1
 
 # Collect
 store_dois <- list()
-for(i in machine_id){
-  #i = 1
+#for(i in machine_id){
+  i = machine_id
   temp_dois <- list()
   for(j in 1:length(store_batches[[i]])){
     #j=1  
     # Process
     temp_dois[[j]] <- get_dois2(issn = store_batches[[i]][j],from_year = 1900,to_year = 2017)
     print(j)
+    save(temp_dois,file=paste0('res_',j,'.Rdata'))
   }
   # Add to big list
-  temp_dois <- temp_dois[sapply(temp_dois,is.data.table)]
-  temp_dois <- rbindlist(temp_dois)
-  store_dois <- rbindlist(list(store_dois,temp_dois))
+ # temp_dois <- temp_dois[sapply(temp_dois,is.data.table)]
+ # temp_dois <- rbindlist(temp_dois)
+ # store_dois <- rbindlist(list(store_dois,temp_dois))
   # Print status
-  print(i)
+ # print(i)
   # Export
-  saveRDS(object = temp_dois,file = paste0('dois_',i,'.rds'))
-  rm(temp_dois)
-}
+  #saveRDS(object = temp_dois,file = paste0('dois_',i,'.rds'))
+ # rm(temp_dois)
+#}
